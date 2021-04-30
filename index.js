ed71c8d1187c3457370e7edd2edfb0fe759f0725
maxrabin <max.rabin17@gmail.com>
2021-04-30 11:49:20 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+function superbowlWin(seasons){
+    console.log(seasons)
+    let answer = seasons.find(function(season){
+        if(season.result === "W"){
+            return true
+        } else {
+            return undefined
+        }
+    })
 
+    return answer !== undefined ? answer.year : undefined
+}
